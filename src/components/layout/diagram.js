ee1dc96b9645e7af8445f5129faa6061f741ead1
Al Nelson <hi@mynameis.al>
2021-05-18 14:11:06 -0700

    webpacker

--- src/components/layout/diagram.js
+++ src/components/layout/diagram.js
@@ -1,7 +1,7 @@
 import * as React from "react"
 import {useState, useRef} from "react"
 import styled from 'styled-components'
-import { graphql, StaticQuery } from 'gatsby'
+import { graphql, StaticQuery } from 'gatsby' 
 import { RichText } from 'prismic-reactjs';
 
 import arrow from '~/images/Links/arrow.svg'
